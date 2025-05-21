41b4e6ab9f4a8abd275691e469f2441423072f58
KNY <gkhgudwp2020@gmail.com>
2025-05-21 19:45:56 +0900

    카카오톡 sdk 추가가

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
+  KakaoSdk.init(nativeAppKey: '${357e8702e8dc4cd5b00e4945d74252d1}');
   runApp(const MainApp());
 }
 
@@ -10,11 +11,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      home: Scaffold(body: Center(child: Text('Hello World!'))),
     );
   }
 }
